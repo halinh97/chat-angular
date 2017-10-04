2288f125962270b79a6f64b742f3aef71a546cdc
VuThuyThuy97 <vuthuy.uet@gmail.com>
2017-10-04 15:12:18 +0700

    right side component

---
+++ source/app/component/rightSide/rightSide.component.js
@@ -0,0 +1,4 @@
+app.component('rightSide',{
+    templateUrl: "component/rightSide/rightSide.view.html",
+    controller: 'rightSideCtrl as rightvm'
+});
---
+++ source/app/component/rightSide/rightSide.controller.js
@@ -0,0 +1,39 @@
+app.controller('rightSideCtrl', rightSideCtrl);
+
+function rightSideCtrl() {
+    var vm = this;
+    vm.conversationName = 'linh tinh';
+    vm.conversationAvatar ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdgU9GP6RHmmVKU5K6u5Fs5K2mgbnQNVqBWE2jrRdcbqPEiREe';
+    vm.info = [
+        {
+            infoName: 'total Message',
+            infoResult: 1000,
+            infoIcon: 'glyphicon glyphicon-envelope'
+        },
+        {
+            infoName: 'total image',
+            infoResult: 100,
+            infoIcon: 'glyphicon glyphicon-camera'
+        },
+        {
+            infoName: 'total file',
+            infoResult: 10,
+            infoIcon: 'glyphicon glyphicon-file'
+        }
+    ];
+    vm.members = [
+        {
+            userName: "Thuy",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdgU9GP6RHmmVKU5K6u5Fs5K2mgbnQNVqBWE2jrRdcbqPEiREe",
+        }, {
+            userName: "Nhan",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdgU9GP6RHmmVKU5K6u5Fs5K2mgbnQNVqBWE2jrRdcbqPEiREe",
+        }, {
+            userName: "Thuy",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdgU9GP6RHmmVKU5K6u5Fs5K2mgbnQNVqBWE2jrRdcbqPEiREe",
+        }, {
+            userName: "Nhan",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdgU9GP6RHmmVKU5K6u5Fs5K2mgbnQNVqBWE2jrRdcbqPEiREe",
+        }
+    ];
+}
